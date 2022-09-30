e79ba8e897ecf69a94a74e699e847d07cd08738b
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-30 09:03:49 +0530

    Employee payroll JS

---
+++ payrollhome.js
@@ -0,0 +1,68 @@
+window.addEventListener('DOMContentLoaded',(event)=>
+{
+    createInnerHtml();
+});
+
+const createInnerHtml=()=>
+{
+    const headerHtml= "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
+    //using template literal
+    let innerHtml= `${headerHtml}`;
+    let empPayrollList= createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+    innerHtml= `${innerHtml}
+    <tr>
+          <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+          <td>${empPayrollData._name}</td>
+          <td>${empPayrollData._gender}</td>
+          <td>${getDeptHtml(empPayrollData._department)}
+          </td>
+          <td>${empPayrollData._salary}</td>
+          <td>${empPayrollData._startDate}</td>
+          <td><img id="${empPayrollData._id}" onclick= "remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img id="${empPayrollData.id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg"></td>
+    </tr>`;
+    }
+    document.querySelector('#table-display').innerHTML=innerHTML;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+      {       
+        _name: 'Harish',
+        _gender: 'male',
+        _department: [
+            'Engineering',
+            'Finance'
+        ],
+        _salary: '500000',
+        _startDate: '29 Oct 2019',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '../assets/profile-images/Ellipse -2.png'
+      },
+      {
+        _name: 'Kumar',
+        _gender: 'female',
+        _department: [
+            'Sales'
+        ],
+        _salary: '400000',
+        _startDate: '29 Oct 2019',
+        _note: '',
+        _id: new Date().getTime() + 1,
+        _profilePic: '../assets/profile-images/Ellipse -1.png'
+      }
+    ];
+    return empPayrollListLocal;
+  }
+
+  const getDeptHtml= (deptList)=>
+  {
+      let deptHtml='';
+      for(const dept of deptList)
+      {
+          deptHtml= `${deptHtml}<div class="dept-label">${dept}</div>`
+      }
+      return deptHtml;
+  }
---
+++ site_properties.js
@@ -0,0 +1,4 @@
+let site_properties={
+    home_page: "../pages/payrollhome.html",
+    emp_payroll_page:"../pages/payrollform.html"
+};
