df2ec802d8e62e5c9b42b859368aff7ada5b3f44
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-30 09:05:31 +0530

    Employee payroll JS

---
+++ site_properties.js
@@ -0,0 +1,5 @@
+//this js file is defined to help reducing hard coded values in app and use dry principle->do not repeat yourself..
+let site_properties={
+    home_page: "../pages/payrollhome.html",
+    emp_payroll_page:"../pages/payrollform.html"
+};
